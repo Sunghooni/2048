7309789cc99a21465cbd24ee5ae8d75d3eb33cbb
SungHoon <leesunghoon03@naver.com>
2021-01-20 23:30:01 +0900

    [ADD] Border_Ctrl
    -Border_Motion Func
    -Border_Prefab

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -13,6 +13,9 @@ public class Map : MonoBehaviour
     [Header("Sources")]
     public GameObject BrightCube;
     public GameObject DarkCube;
+    public GameObject BorderPrefab;
+
+    private float cubeWidth;
 
     public int MapSize
     {
@@ -28,6 +31,7 @@ public class Map : MonoBehaviour
 
     private void Awake()
     {
+        cubeWidth = BrightCube.transform.localScale.x;
         StartCoroutine(SetBoard());
     }
 
@@ -39,14 +43,40 @@ public class Map : MonoBehaviour
         {
             for (int j = 0; j < MapSize; j++)
             {
-                cubePos.x = j * 3; //BrightCube.transform.localScale.x
-                cubePos.y = i * 3;
+                cubePos.x = j * cubeWidth;
+                cubePos.y = i * cubeWidth;
 
-                var cubeType = (i + j) % 2 == 0 ? BrightCube : DarkCube;
-                Instantiate(cubeType, cubePos, Quaternion.Euler(Vector3.zero));
+                GameObject cubeType = (i + j) % 2 == 0 ? BrightCube : DarkCube;
+                Instantiate(cubeType, cubePos, Quaternion.identity);
 
                 yield return new WaitForSeconds(0.3f / MapSize);
             }
+        }
+        StartCoroutine(SetBorder());
+        yield break;
+    }
+
+    IEnumerator SetBorder()
+    {
+        float halfWidth = cubeWidth / 2;
+        float centerWidth = mapSize * halfWidth - halfWidth;
+
+        Vector3 startPos = new Vector3(centerWidth, centerWidth, -2);
+        Vector3 toPos = new Vector3(centerWidth, centerWidth, 10);
+
+        GameObject Border = Instantiate(BorderPrefab, startPos, Quaternion.identity);
+        Border.transform.localScale = new Vector3(MapSize, MapSize, 1);
+
+        while (true)
+        {
+            if (Border.transform.position.z >= toPos.z)
+            {
+                Debug.Log("MapSetting Finished");
+                break;
+            }
+
+            Border.transform.position = Vector3.Lerp(Border.transform.position, toPos, Time.deltaTime * 3);
+            yield return new WaitForFixedUpdate();
         }
 
         yield break;
