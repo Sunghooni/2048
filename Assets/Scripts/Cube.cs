29c9d68ed1527d87714c0be1def4fa474991b99b
SungHoon <leesunghoon03@naver.com>
2021-01-30 00:36:34 +0900

    [ROLLBACK] Cube이동 관련 스크립트 초기화
    - 설계 오류로 롤백

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public int value;
-    public bool collapable = false;
-    public bool checkTurn = false;
 
     private void Awake()
     {
@@ -17,33 +15,5 @@ public class Cube : MonoBehaviour
     {
         int random = Random.Range(0, 10);
         value = random < 1 ? 4 : 2;
-    }
-
-    public void Move(Vector3 toPos)
-    {
-        StartCoroutine(MoveMotion(toPos));
-    }
-
-    IEnumerator MoveMotion(Vector3 toPos)
-    {
-        float timer = 0;
-        Vector3 originPos = gameObject.transform.position;
-
-        while(timer <= 1)
-        {
-            timer += Time.deltaTime;
-            gameObject.transform.position = Vector3.Lerp(originPos, toPos, timer);
-
-            yield return new WaitForFixedUpdate();
-        }
-        if(collapable)
-        {
-            Debug.Log("Collab");
-        }
-        if(checkTurn)
-        {
-
-        }
-        yield return null;
     }
 }
--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -20,7 +20,6 @@ public class CubeManager : MonoBehaviour
 
         SetNewCube();
         SetNewCube();
-        MoveCtrl("Right");
     }
 
     //새로운 큐브 생성 관련 함수들
@@ -71,55 +70,5 @@ public class CubeManager : MonoBehaviour
     }
 
     //입력에 따른 이동명령
-    public void MoveCtrl(string dir)
-    {
-        for(int i = 0; i < mapSize; i++)
-        {
-            for(int j = mapSize - 2; j >= 0; j--)
-            {
-                if (CubeArray[i, j] == null)
-                {
-                    continue;
-                }
-
-                GameObject compareCube = CompareCube(dir, i, j);
-                Vector3 comparePos = ComparePos(dir, i, j);
-
-                if (compareCube == null)
-                {
-                    CubeArray[i, j].GetComponent<Cube>().Move(comparePos);
-                }
-                else
-                {
-                    var thisCube = CubeArray[i, j].GetComponent<Cube>();
-                    var toCube = compareCube.GetComponent<Cube>();
-
-                    if(thisCube.value == toCube.value)
-                    {
-                        thisCube.Move(comparePos);
-                        thisCube.collapable = true;
-                        thisCube.value = thisCube.value * 2;
-                    }
-                    else continue;
-                }
-                CubeArray[i, j + 1] = CubeArray[i, j];
-                CubeArray[i, j] = null;
-            }
-        }
-    }
-
-    //상하좌우 이동 방향에 따라 비교할 대상 반환
-    private GameObject CompareCube(string dir, int i, int j)
-    {
-        if (dir == "Right") return CubeArray[i, j + 1];
-
-        return null;
-    }
-
-    private Vector3 ComparePos(string dir, int i, int j)
-    {
-        if (dir == "Right") return new Vector3((j + 1) * cubeWidth, i * -cubeWidth, 9.5f);
-        
-        return Vector3.zero;
-    }
+    
 }
