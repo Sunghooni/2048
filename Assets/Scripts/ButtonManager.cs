af18ce2f4ac2e4b2e5dc45204f58f5d9bd3ecb0c
SungHoon <leesunghoon03@naver.com>
2021-03-04 13:23:42 +0900

    [ADD] Button Images

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void HomeBtnPressed()
+    {
+
+    }
+
+    public void RetryBtnPressed()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(gameObject.scene.name);
+    }
+
+    public void RankBtnPressed()
+    {
+
+    }
+}
