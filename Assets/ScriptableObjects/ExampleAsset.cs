422007b43f08973a5003301367dc91035f004eb3
SungHoon <leesunghoon03@naver.com>
2021-03-05 12:58:17 +0900

    [ADD] Sample : Scriptable Object

---
+++ Assets/ScriptableObjects/ExampleAsset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExampleAsset : ScriptableObject
+{
+    [SerializeField]
+    private string str;
+
+
+    [MenuItem("Example/Create ExampleAsset Instance")]
+    static void CreateExampleAssetInstance()
+    {
+        var exampleAsset = CreateInstance<ExampleAsset>();
+    }
+
+    [MenuItem("Example/Create ExampleAsset")]
+    static void CreateExampleAsset()
+    {
+        var exampleAsset = CreateInstance<ExampleAsset>();
+
+        AssetDatabase.CreateAsset(exampleAsset, "Assets/ScriptableObjects/ExampleAsset.asset");
+        AssetDatabase.Refresh();
+    }
+}
