4bf4fd795f3bf787dcf18e11702ac11ea0200340
SungHoon <leesunghoon03@naver.com>
2021-03-16 17:03:01 +0900

    [ALERT] Localhost사용 관련 경고

--- Assets/Scripts/Server/GetRankInfo.cs
+++ Assets/Scripts/Server/GetRankInfo.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class GetRankInfo : MonoBehaviour
 {
+	//원래 localhost대신에 서버ip주소를 적어야 함
+	//하지만 그럴 경우에는 인증서 허가가 추가로 필요해 localhost를 사용했음
 	public string baseURL = "https://localhost:44310/api/RankInfo";
 
 	public RankInfo GetRankData()
